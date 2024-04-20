b42cc269fd59bf56780b08920912e2731a7850ca
Jay Tun <146002359+jay-tun@users.noreply.github.com>
2024-04-20 20:35:22 +0200

    Add files via upload
    This file splits the given JAAD dataset into training dataset and testing dataset to train the model on motion prediction and ultimately use it to test it.

---
+++ split_data.py
@@ -0,0 +1,29 @@
+from sklearn.model_selection import train_test_split
+import os
+import shutil
+
+
+data_folder = '/home/jnomad/Documents/Semester 6/Thesis/GRIP-master/JAAD_clips/'
+
+video_clips = os.listdir(data_folder)
+
+train_clips, test_clips = train_test_split(video_clips, test_size=0.33, random_state=47)
+
+train_folder = '/home/jnomad/Documents/Semester 6/Thesis/GRIP-master/train_foler'
+test_folder = '/home/jnomad/Documents/Semester 6/Thesis/GRIP-master/test_folder'
+
+
+#create the train and test folders if they don't already exist
+os.makedirs(train_folder, exist_ok=True)
+os.makedirs(test_folder, exist_ok=True)
+
+#move training clips to the train folder
+for clip in train_clips:
+	shutil.move(os.path.join(data_folder, clip), os.path.join(train_folder, clip))
+
+#move testing clips to the test folder
+for clip in test_clips:
+	shutil.move(os.path.join(data_folder, clip), os.path.join(test_folder, clip))
+
+print("{} - training clips and {} - testing clips.".format(len(train_clips), len(test_clips)))
+
