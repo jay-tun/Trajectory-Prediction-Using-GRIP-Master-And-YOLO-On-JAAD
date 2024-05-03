453eb903f3e6b2f27654f19fcd44e456b24e5e52
Jay Tun <146002359+jay-tun@users.noreply.github.com>
2024-05-03 16:20:39 +0200

    Add files via upload

---
+++ data_prepare/erase.py
@@ -0,0 +1,17 @@
+import os
+
+def delete_empty_files(directory):
+    # Iterate over all files in the directory
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        
+        # Check if the file is empty
+        if os.path.isfile(filepath) and os.path.getsize(filepath) == 0:
+            # Delete the empty file
+            os.remove(filepath)
+            print(f"Deleted empty file: {filepath}")
+
+# Example usage:
+directory_path = "/home/jnomad/Documents/Semester 6/Thesis/GRIP-master/tess/train/"
+delete_empty_files(directory_path)
+
